0090f1a9f8c55fd75cc2aacf91ec5857e6cc9765
LinhLuu254 <llinh254@gmail.com>
2023-04-09 16:37:14 -0400

    cocktail portfolio

---
+++ enabler.js
@@ -0,0 +1,18 @@
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+    );
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+    });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+      $('.toast').toast('show');
+    });
+  });
